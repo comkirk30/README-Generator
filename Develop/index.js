f35e374cb962ee39cf896dd79b92cf87bc046929
Courtney <courtneymkirk@hotmail.com>
2022-01-31 20:35:05 -0500

    input questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,75 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const ReadMeTemplate = require("./src/ReadMeTemplate");
 
 // TODO: Create a function to initialize app
-function init() {}
+const createFile = util.promisify(fs.writeFile);
+
+
+// TODO: Create an array of questions for user input
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type:'input',
+            name:'username',
+            message:'Enter GitHub username:'
+        },
+        {
+            type:'input',
+            name:'projectTitle',
+            message:'What is the project title?'
+        },
+        {
+            type:'input',
+            name:'description',
+            message:'Describe the project:'
+        },
+        {
+            type:'input',
+            name:'installation',
+            message:'What are the steps required to install your project?'
+        },
+        {
+            type:'input',
+            name:'usage',
+            message:'Provide instructions and examples for use. Include screenshots as needed:'
+        },
+        {
+            type:'input',
+            name:'credits',
+            message:'List your collaborators, if any. If none, skip or type "N/A":'
+        },
+        {
+            type:'list',
+            name:'license',
+            message:'Choose the license for this project:',
+            choices: ['GNU AGPL', 'GNU LGPL', 'Mozilla', 'Apache','MIT', 'Boost Software','The Unlicense']
+        },
+        {
+            type:'input',
+            name:'features',
+            message:'List project features/languages:'
+        },
+        {
+            type:'input',
+            name:'contributing',
+            message:'If you would like others to contribute to your app/package, add guidelines for how to do so. If not, skip or type "N/A":'
+        },
+    ])
+};
+
+
+
+
+
+
+
 
 // Function call to initialize app
 init();
